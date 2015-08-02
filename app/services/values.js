66ada7aff0d4d8ff78a36e9c187f34a3bb9614ed
LPayyapilli <luke.payyapilli@gmail.com>
2015-08-02 17:20:42 -0400

    tries to fix error in url for messages

--- app/services/values.js
+++ app/services/values.js
@@ -2,5 +2,6 @@
 angular.module("spacin").value("appSettings", {
   title: "Spacin",
   version: "1.0",
-  url: 'http://spacin.herokuapp.com'
+  // url: 'http://spacin.herokuapp.com'
+  url: 'http://localhost:3000'
 });
